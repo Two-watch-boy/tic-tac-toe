caeb2e976fa1c3aea908da9ac9ecde7b53cc87ca
Two-Watch-Boy <twowatchboy@gmail.com>
2016-03-19 15:33:48 -0700

    board and x's

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 // wait for the DOM to finish loading
-$(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+console.log("this is a sanity check");
 
+$(".btn").on("mousedown",function(){
+  for (var i=0; i<10; i++){
+    if (i%2===0){
+      $(this).text("X");
+    }else if (i%2===1){
+      $(this).text("O");
+    }
+      console.log(i);
+}
 });
