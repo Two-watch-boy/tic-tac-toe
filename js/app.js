6395b2c0d267e95d6b554fd35baf78fbd39b5ec0
Two-Watch-Boy <twowatchboy@gmail.com>
2016-03-20 11:13:02 -0700

    reset button and full board

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ console.log("this is a sanity check2");
 // the global move number
 window.onload= function(){
   var i = 0;
-  $(".btn").on("mousedown",function(event){
+  $(".game").on("mousedown",function(event){
     console.log("this is my event.target " + $(event.target).text());
     if ($(event.target).text() === " ") {
       if (i % 2 === 1){
@@ -15,9 +15,20 @@ window.onload= function(){
           console.log("x");
       }
       i++;
+      console.log (i);
+      if(i === 9){
+        alert("Sad Panda, No More Spaces!");
+      }
     }else{
-      alert("Space Taken");
+      alert("Space Taken, Please Choose Another");
     }
+  });//closes mousedown
+
+  $(".reset").on("mousedown",function (){
+    $(".game").text(" ");
+    i = 0;
+    console.log ("calling all x's and o's", i);
   });
-};//end window onload
+};
+//end window onload
 console.log("end of sanity check2");
