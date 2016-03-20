4c838830360aa7c94b0ba5c86a25c36d88f55fbb
Two-Watch-Boy <twowatchboy@gmail.com>
2016-03-20 10:23:02 -0700

    space taken pop-up

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 // wait for the DOM to finish loading
-console.log("this is a sanity check");
+console.log("this is a sanity check2");
 
-$(".btn").on("mousedown",function(){
-  for (var i=0; i<10; i++){
-    if (i%2===0){
-      $(this).text("X");
-    }else if (i%2===1){
-      $(this).text("O");
+// the global move number
+window.onload= function(){
+  var i = 0;
+  $(".btn").on("mousedown",function(event){
+    console.log("this is my event.target " + $(event.target).text());
+    if ($(event.target).text() === " ") {
+      if (i % 2 === 1){
+          $(this).text("O");
+          console.log("o");
+      }else{
+          $(this).text("X");
+          console.log("x");
+      }
+      i++;
+    }else{
+      alert("Space Taken");
     }
-      console.log(i);
-}
-});
+  });
+};//end window onload
+console.log("end of sanity check2");
